19f08acb2145faa261f44bb6de8cb2f6cef4370e
Ali Ibtehaj Asif Niazi <aliibtehajasifniazi@gmail.com>
2024-03-04 12:32:34 +0000

    Loaded the data from both Bookings and Flights file and saved it in a HashMap. Also added check for correct booking reference

--- AirportCheckIn.java
+++ AirportCheckIn.java
@@ -4,24 +4,45 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class AirportCheckIn {
 	public File bookingsFile;
 	public File flightsFile;
-	public List<Booking> bookings;
-	public List<Flight> flights;
-	
+	public HashMap<String, Booking> bookings;
+	public HashMap<String, Flight> flights;
+
+	public AirportCheckIn() {
+		bookings = new HashMap<>();
+		flights = new HashMap<>();
+    }
+
 	public void loadBookingsFromFile() {
 		String line = "";  
-		bookings = new ArrayList<Booking>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("bookings.txt"));  
 			while ((line = br.readLine()) != null)
 			{  
 				String[] fileLine = line.split(",");
-				boolean checkedIn = fileLine[3] == "yes";
+				if (!isValidBookingReference(fileLine[0])) {
+                    System.out.println("Invalid booking reference: " + fileLine[0]);
+                    System.out.println("Please correct the file and load again.");
+                    return; 
+                }
+				boolean checkedIn = fileLine[3].equals("true");
 				Booking newBooking = new Booking(fileLine[0],fileLine[1],fileLine[2],checkedIn);
-				bookings.add(newBooking);
+				bookings.put(fileLine[0], newBooking);
+			}
+			for (HashMap.Entry<String, Booking> entry : bookings.entrySet()) {
+				String key = entry.getKey();
+				Booking booking = entry.getValue();
+				String bookingInfo = "Booking ID: " + key +
+									", Passenger Name: " + booking.getPassengerName() +
+									", Flight Code: " + booking.getFlightCode() +
+									", Checked In: " + booking.isCheckedIn();
+				System.out.println(bookingInfo);
 			}
 			br.close();
 		}
@@ -29,17 +50,30 @@ public class AirportCheckIn {
 			e.printStackTrace();
 		}
 	}
+	private boolean isValidBookingReference(String bookingRef) {
+        Pattern pattern = Pattern.compile("^[A-Z]{3}\\d{3}$");
+        Matcher matcher = pattern.matcher(bookingRef);
+        return matcher.matches();
+    }
 	
 	public void loadFlightsFromFile() {
 		String line = "";  
-		flights = new ArrayList<Flight>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("flights.txt"));  
 			while ((line = br.readLine()) != null)
 			{  
 				String[] fileLine = line.split(",");
-				Flight newFlight = new Flight(fileLine[0],fileLine[1],Integer.parseInt(fileLine[2]),Double.parseDouble(fileLine[3]),Double.parseDouble(fileLine[3]));
-				flights.add(newFlight);
+				Flight newFlight = new Flight(fileLine[0],fileLine[1],fileLine[2],Integer.parseInt(fileLine[3]),Double.parseDouble(fileLine[4]),Double.parseDouble(fileLine[5]));
+				flights.put(fileLine[0], newFlight);
+			}
+			for (HashMap.Entry<String, Flight> entry : flights.entrySet()) {
+				String key = entry.getKey();
+				Flight flight = entry.getValue();
+				String flightInfo = "FLight ID: " + key +
+									", Des: " + flight.getDestinationAirport() +
+									", carrie: " + flight.getCarrier() +
+									", Capacity: " + flight.getCapacity();
+				System.out.println(flightInfo);
 			}
 			br.close();
 		}
